c7ceabe0a890a6e3469a1b9f9f3da8865bfddeaa
Catz <catnyan2002@gmail.com>
2021-06-13 22:25:04 +0300

    First Documentation.

--- src/lib.hpp
+++ src/lib.hpp
@@ -2,38 +2,49 @@
 #include <string>
 #include <vector>
 
-
+/// Crypto Currency
+/**
+ * Creates crypto coin. Coin stores its name and parameters that account for value.
+ */
 struct crypto_currency{
-    std::string coin_name;
-    double market_cap;
-    double circulating_sup;
-    std::vector<float> daily_values;
+    std::string coin_name; ///< coin's name
+    double market_cap; ///< market capitalization of the coin
+    double circulating_sup; ///< circulating supply of the coin
+    std::vector<float> daily_values; ///< daily values of the coin for a period of time
 
-    auto daily_fluctuations() -> bool; //changes market_cap and circulating_sup of a coin
-    auto count_value() -> float; //counts today's value of a coin
-    auto update_values(float val) -> void; //adds value to daily_values
-    auto init_fluctuations() -> bool; //creates daily value for 6 previous days
+    auto daily_fluctuations() -> bool; ///< randomly changes market capitalization and circulating supply of the coin
+    auto count_value() -> float; ///< returns current value of a coin
+    auto update_values(float val) -> void; ///< adds value to daily_values
+    auto init_fluctuations() -> bool; ///< creates daily_values for 6 days prior to gameplay and for the first day of the gameplay
 };
 
+/// Crypto Wallet
+/**
+ * Creates crypto wallet for user. Crypto wallet can have various coins.
+ */
 struct crypto_balance{
-    std::vector<crypto_currency> coins;
-    std::vector<float> balance_by_coin;
+    std::vector<crypto_currency> coins; ///< vector with coins that you want to add to the wallet
+    std::vector<float> balance_by_coin; ///< vector with number of respective coins that user currently has
 
-    auto coin_name(unsigned int coin_number) -> std::string; //allows to look up the name of the coin by its index in balance_by_coin
-    auto add_coins(unsigned int coin, float val) -> bool; //allows to add chosen coins to the wallet
-    auto withdraw_coins(unsigned int coin, float val) -> bool; //allows to withdraw chosen coins from the wallet
+    auto coin_name(unsigned int coin_number) -> std::string; ///< looks up the name of the coin by its index in balance_by_coin
+    auto add_coins(unsigned int coin, float val) -> bool; ///< adds chosen number of coins to respective coin in the user's wallet
+    auto withdraw_coins(unsigned int coin, float val) -> bool; ///< withdraws chosen number of coins from respective coin in the user's wallet
 };
 
+/// User
+/**
+ * Initializes User. User has a wallet with different types of coins and a startup capital.
+ */
 struct user {
-    crypto_balance wallet;
-    float balance;
-    int day;
+    crypto_balance wallet; ///< user's wallet with crypto coins
+    float balance; ///< startup capital of user
+    int day; ///< current day of gameplay
 
-    auto next_day() -> void; //allows to go to the next day of trading coins
-    auto current_value(unsigned int coin) -> float; //allows to look up the current value of a coin
-    auto add_coins(unsigned int coin, float val) -> bool; //allows to add chosen coins to the wallet
-    auto withdraw_coins(unsigned int coin, float val) -> bool; //allows to withdraw chosen coins from the wallet
-    auto count_profit() -> float; //allows to count profit
-    auto score() -> std::string; // allows to count the score of a player
-    auto end_of_the_week() -> bool; //generates information for last day week recap
+    auto next_day() -> void; ///< goes to the next day of trading operations
+    auto current_value(unsigned int coin) -> float; ///< looks up current value of a coin
+    auto add_coins(unsigned int coin, float val) -> bool; ///< buys chosen coin and the adds it to the wallet
+    auto withdraw_coins(unsigned int coin, float val) -> bool; ///< sells chosen coin and withdraws it from the wallet
+    auto count_profit() -> float; ///< counts profit
+    auto score() -> std::string; ///< counts user's (player's) score
+    auto end_of_the_week() -> bool; ///< generates information for last day week recap
 };
