460077c83495ae28764de9d12e9007bdd997a926
Kira <kiratechno@pm.me>
2021-06-13 15:44:39 +0300

    Add external libs (nana, doctest)

---
+++ external/doctest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kira/CLionProjects/CryptoTrade/cpp-starter-project-master/external/doctest
+# Build directory: /home/kira/CLionProjects/CryptoTrade/cpp-starter-project-master/external/doctest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
