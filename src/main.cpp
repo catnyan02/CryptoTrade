eb72689d97b3f1ebfd4e802483fe3af1c1437054
Kira <kiratechno@pm.me>
2021-06-17 15:16:36 +0300

    some text ui done

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 
 #include "lib.hpp"
+#include <array>  // for array
+#include <chrono> // for operator""s, chrono_literals
+#include <cmath>  // for sin
+#include <cstdarg>
 #include <experimental/random>
+#include <functional> // for ref, reference_wrapper, function
 #include <iostream>
+#include <memory>  // for allocator, shared_ptr, __shared_ptr_access
+#include <string>  // for wstring, basic_string, operator+, to_wstring
+#include <thread>  // for sleep_for, thread
+#include <utility> // for move
+#include <vector>  // for vector
 
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
+#include "ftxui/component/captured_mouse.hpp" // for ftxui
+#include "ftxui/component/component.hpp" // for Checkbox, Renderer, Horizontal, Vertical, Input, Menu, Radiobox, Tab, Toggle
+#include "ftxui/component/component_base.hpp" // for ComponentBase
+#include "ftxui/component/event.hpp"          // for Event, Event::Custom
+#include "ftxui/component/input.hpp"          // for InputBase
+#include "ftxui/component/screen_interactive.hpp" // for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp" // for text, operator|, color, bgcolor, Element, filler, size, vbox, flex, hbox, graph, separator, EQUAL, WIDTH, hcenter, bold, border, window, HEIGHT, Elements, hflow, flex_grow, frame, gauge, LESS_THAN, spinner, dim, GREATER_THAN
+#include "ftxui/screen/color.hpp" // for Color, Color::BlueLight, Color::RedLight, Color::Black, Color::Blue, Color::Cyan, Color::CyanLight, Color::GrayDark, Color::GrayLight, Color::Green, Color::GreenLight, Color::Magenta, Color::MagentaLight, Color::Red, Color::White, Color::Yellow, Color::YellowLight, Color::Default
 
 float INITIAL_BALANCE = 1000000;
 
@@ -115,7 +131,7 @@ auto user::add_coins(unsigned int coin, float val) -> bool {
 auto user::withdraw_coins(unsigned int coin, float val) -> bool {
     if (wallet.withdraw_coins(coin, val)) {
         auto coin_value = current_value(coin);
-        balance += coin_value * val;
+        balance -= coin_value * val;
         return true;
     }
     return false;
@@ -167,19 +183,194 @@ int main(int argc, char *argv[]) {
     user new_user{user_balance, INITIAL_BALANCE, 0};
     using namespace ftxui;
 
-    Element doc1 = hbox({vbox({
-                             text(L"middler") | border | flex,
-                             text(L"middle") | border | flex,
-                         }) | border |
-                             flex,
-                         text(L"OOOOO") | border | flex});
-    // Define the document
+    std::vector<std::wstring> coins_label = {
+        L"krsch_coin", L"kzmnv_coin", L"bitcoin", L"etherium",
+        L"dogecoin",   L"ravencoin",  L"dodo",
+    };
 
-    auto screen = Screen::Create(Dimension::Full(),   // Width
-                                 Dimension::Fit(doc1) // Height
-    );
-    Render(screen, doc1);
-    screen.Print();
+    auto screen = ScreenInteractive::Fullscreen();
 
-    return EXIT_SUCCESS;
+    int shift = 0;
+
+    const std::vector<std::wstring> trading_entries = {
+        L"withdraw",
+        L"buy",
+    };
+
+    int trading_selected = 0;
+    Component trading = Radiobox(&trading_entries, &trading_selected);
+
+    std::array<bool, 7> options_state = {
+        false, false, false, false, false, false, false,
+    };
+    std::wstring input_add_content;
+    Component input_add = Input(&input_add_content, "input files");
+
+    std::vector<std::wstring> input_entries;
+    int input_selected = 0;
+    Component input = Menu(&input_entries, &input_selected);
+
+    std::wstring amount_content_ = L"";
+    Component amount_ = Input(&amount_content_, "amount");
+
+    int coin_selected = 0;
+    Component coins = Container::Vertical({
+        Radiobox(&coins_label, &coin_selected),
+        // Checkbox(&options_label[0], &options_state[0]),
+        //     Checkbox(&options_label[1], &options_state[1]),
+        //     Checkbox(&options_label[2], &options_state[2]),
+        //     Checkbox(&options_label[3], &options_state[3]),
+        //     Checkbox(&options_label[4], &options_state[4]),
+        //     Checkbox(&options_label[5], &options_state[5]),
+        //     Checkbox(&options_label[6], &options_state[6]),
+    });
+
+    Component markets = Radiobox(&coins_label, &coin_selected);
+    Component rates = Container::Vertical({});
+
+    auto markets_component = Container::Horizontal({
+        Container::Vertical({markets}),
+        Container::Vertical({rates}),
+    });
+
+    // auto coin_data_component = Container::Horizontal(
+    // );
+
+    auto trading_component = Container::Horizontal({
+        trading,
+        coins,
+        Container::Vertical({
+            amount_,
+            Container::Horizontal({
+                input_add,
+                input,
+            }),
+        }),
+    });
+
+    InputBase::From(input_add)->on_enter = [&] {
+        input_entries.push_back(input_add_content);
+        input_add_content = L"";
+    };
+
+    auto render_command = [&] {
+        Elements line;
+        // Trading
+        line.push_back(text(trading_entries[trading_selected]) | bold);
+        // flags
+        line.push_back(text(L" "));
+        line.push_back(text(coins_label[coin_selected]) | dim);
+        // Amount
+        if (!amount_content_.empty()) {
+            line.push_back(text(L" ") | bold);
+            line.push_back(text(amount_content_) | color(Color::BlueLight) |
+                           bold);
+        }
+        // Input
+        for (auto &it : input_entries) {
+            line.push_back(text(L" " + it) | color(Color::RedLight));
+        }
+        return line;
+    };
+
+    auto trading_renderer = Renderer(trading_component, [&] {
+        auto trading_win =
+            window(text(L"Trading mode"), trading->Render() | frame);
+        auto coins_win = window(text(L"Coins"), coins->Render());
+        auto amount_win = window(text(L"Amount:"), amount_->Render());
+        auto input_win =
+            window(text(L"Input"),
+                   hbox({
+                       vbox({
+                           hbox({
+                               text(L"Add: "),
+                               input_add->Render(),
+                           }) | size(WIDTH, EQUAL, 20) |
+                               size(HEIGHT, EQUAL, 1),
+                           filler(),
+                       }),
+                       separator(),
+                       input->Render() | frame | size(HEIGHT, EQUAL, 3) | flex,
+                   }));
+        return vbox({
+                   hbox({
+                       trading_win | size(HEIGHT, LESS_THAN, 6),
+                       coins_win,
+                       vbox({
+                           amount_win | size(WIDTH, EQUAL, 20),
+                           input_win | size(WIDTH, EQUAL, 60),
+                       }),
+                       filler(),
+                   }),
+                   hflow(render_command()) | flex_grow,
+               }) |
+               flex_grow | border;
+    });
+
+    auto coin_data_renderer = Renderer([&] {
+        auto rates = vbox({});
+        auto coin_names = vbox({
+            text(coins_label[0]),
+            text(coins_label[1]),
+            text(coins_label[2]),
+            text(coins_label[3]),
+            text(coins_label[4]),
+            text(coins_label[5]),
+            text(coins_label[6]),
+        });
+        return vbox({
+                   hbox({
+                       vbox({
+                           coin_names | size(WIDTH, EQUAL, 50),
+                       }),
+                       vbox({
+                           rates | size(WIDTH, EQUAL, 50),
+                       }),
+                       filler(),
+                   }),
+                   hflow(render_command()) | flex_grow,
+               }) |
+               flex_grow | border;
+    });
+
+    int tab_index = 0;
+    std::vector<std::wstring> tab_entries = {
+        L"trade",
+        L"coin data",
+        L"profile",
+    };
+    auto tab_selection = Toggle(&tab_entries, &tab_index);
+    auto tab_content = Container::Tab(
+        {
+            trading_renderer,
+            coin_data_renderer,
+
+        },
+        &tab_index);
+
+    auto main_container = Container::Vertical({
+        tab_selection,
+        tab_content,
+    });
+
+    auto main_renderer = Renderer(main_container, [&] {
+        return vbox({
+            text(L"CryptoTrade") | bold | hcenter,
+            tab_selection->Render() | hcenter,
+            tab_content->Render() | flex,
+        });
+    });
+
+    std::thread update([&screen, &shift]() {
+        for (;;) {
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(0.05s);
+            shift++;
+            screen.PostEvent(Event::Custom);
+        }
+    });
+
+    screen.Loop(main_renderer);
+
+    return 0;
 }
