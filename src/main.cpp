83dfd492aa9fb1c9460fb7ffea56bcdc951e180b
Catz <catnyan2002@gmail.com>
2021-06-17 17:05:29 +0300

    Small changes to calculating formulas.

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ double COIN_SUP_6 = 110551965;
 
 auto crypto_currency::daily_fluctuations() -> bool {
     std::experimental::reseed();
-    int range_max_cap = static_cast<int>(market_cap * 0.0001);
+    int range_max_cap = static_cast<int>(market_cap * 0.01);
     int range_max_sup = static_cast<int>(circulating_sup);
     int random_cap = std::experimental::randint(0, range_max_cap);
     std::experimental::reseed();
